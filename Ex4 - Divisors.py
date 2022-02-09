e93547233a8c117e0177b021a40762a15c5dd708
Nishant <nishant.amalik@gmail.com>
2022-02-09 21:28:16 +0000

    Add files via upload

---
+++ Ex4 - Divisors.py
@@ -0,0 +1,20 @@
+def divisor(number):
+  new_list=[]
+  x = range(1,((number/2)+1))
+  for elem in x:
+    if number%elem==0:
+      new_list.append(elem)
+  
+  return new_list    
+
+def main():
+  
+  number = input("Enter any integer of you choice: ")
+  print number, type(number)
+  assert number>0 and type(number)!="int","Wrong input"
+  print divisor(number)
+  
+main()  
+  
+
+https://gist.github.com/anonymous/ec91975707af033572077c2f70947283
