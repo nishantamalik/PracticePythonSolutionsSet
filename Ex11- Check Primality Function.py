75c2440bf4de40c3fb9b384e671feb63917d12e2
Nishant <nishant.amalik@gmail.com>
2022-02-12 14:58:26 +0000

    Add files via upload

---
+++ Ex11- Check Primality Function.py
@@ -0,0 +1,14 @@
+def prime(num):
+  new_list = [x for x in range(2,((num/2)+1)) if num%x == 0]
+  if new_list == []:
+    print "Prime"
+  else:
+    print "Not Prime"
+    
+def main():
+  prime(int(input("Enter any number: ")))
+  
+main()  
+
+
+https://gist.github.com/anonymous/a5aa3519a6d0ce99ed00605f531835f2
