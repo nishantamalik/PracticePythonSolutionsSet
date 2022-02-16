03733ce3a86e3be2ffce54ba1b429c83122aeb22
Nishant <nishant.amalik@gmail.com>
2022-02-16 13:13:16 +0000

    Add files via upload

---
+++ Ex15 - Reverse Word Order.py
@@ -0,0 +1,14 @@
+def reverse_word(input_str):
+  output_str = ""
+  new_list = input_str.split()
+  output_str = " ".join(new_list[::-1])
+  return output_str
+  
+
+def main():
+  input_str = input("Enter any sentence: ")
+  print reverse_word(input_str)
+  
+main()  
+
+https://gist.github.com/anonymous/8befd3e9cd954d0690651bbfeeeb9515
